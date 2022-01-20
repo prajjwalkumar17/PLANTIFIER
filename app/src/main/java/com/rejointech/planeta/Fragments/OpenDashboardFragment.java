5338c2347b1a66d03a33ee119072651f1204d920
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-01-21 03:02:24 +0530

    dashboard made with image slider on one
    Signed-off-by: prajjwalkumar17 <talk2prajjwal@gmail.com>

---
+++ app/src/main/java/com/rejointech/planeta/Fragments/OpenDashboardFragment.java
@@ -0,0 +1,23 @@
+package com.rejointech.planeta.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.rejointech.planeta.R;
+
+public class OpenDashboardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_open_dashboard, container, false);
+
+
+        return root;
+    }
+}
